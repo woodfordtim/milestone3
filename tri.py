dfebf3237d6191e85e32e7a86fa03124279f87ea
woodfordtim <woodfordtim@gmail.com>
2020-04-09 21:32:11 +0000

    Add sumbit create event form data

--- tri.py
+++ tri.py
@@ -14,15 +14,25 @@ app.config["MONGODB_NAME"] = os.environ.get('MONGODB_NAME')
 
 mongo = PyMongo(app)
 
+# function for page to list all events
 @app.route('/')
 @app.route('/find_events')
 def find_events():
     return render_template("events.html", events=mongo.db.events.find())
 
+# function for form to add events
 @app.route('/add_event')
 def add_event():
     return render_template('addevent.html', 
     event_type=mongo.db.event_type.find()) # event_type refers to Collection so must be accurate
+
+# function to sumbit a form http method POST - default is GET
+@app.route('/create_event', methods=['POST'])
+def create_event():
+    events=mongo.db.events
+    events.insert_one(request.form.to_dict()) #insert when you submit info to a uri it does so in a request object and then convert the form to dictionary so it can be understood by MongoDB
+    return redirect(url_for('/find_events'))
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
