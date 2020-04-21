3f1c8602b6a8e573941eee4af23bcad57fbddb2b
woodfordtim <woodfordtim@gmail.com>
2020-04-21 21:11:24 +0000

    add edit event function

--- tri.py
+++ tri.py
@@ -23,7 +23,7 @@ def find_events():
 # function for form to add events
 @app.route('/add_event')
 def add_event():
-    return render_template('addevent.html', 
+    return render_template('add_event.html', 
     event_type=mongo.db.event_type.find()) # event_type refers to Collection so must be accurate
 
 # function to sumbit a form http method POST - default is GET
@@ -32,6 +32,32 @@ def create_event():
     events=mongo.db.events
     events.insert_one(request.form.to_dict()) #insert when you submit info to a uri it does so in a request object and then convert the form to dictionary so it can be understood by MongoDB
     return redirect(url_for('/find_events'))
+
+@app.route('/edit_event/<event_id>')
+def edit_event(event_id):
+    the_event = mongo.db.events.find_one({"_id": ObjectId(event_id)})
+    all_event_type = mongo.db.event_type.find()
+    return render_template('edit_event.html', event=the_event, event_type=all_event_type)
+
+@app.route('/update_event/<event_id>', methods=['POST']) 
+def update_event(event_id):
+    events = mongo.db.events
+    events.update( {'_id': ObjectId(event_id)},
+    {
+        'event_name': request.form.get('event_name'),
+        'event_type': request.form.get('event_type'),
+        'event_distance': request.form.get('event_distance'),
+        'cost': request.form.get('cost'),
+        'difficulty': request.form.get('difficulty'),
+        'location_url': request.form.get('location_url'),
+        'event_date': request.form.get('event_date'),
+        'last_updated': request.form.get('last_updated'),
+        'event_contacts': request.form.get('event_contacts'),
+        'author': request.form.get('author'),
+        'link': request.form.get('link')
+    })
+    return redirect(url_for('find_events'))
+
 
 
 if __name__ == '__main__':
