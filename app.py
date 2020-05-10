fd167fc420fa15bc30a52e651ba4f0c2ef0135fe
woodfordtim <woodfordtim@gmail.com>
2020-05-10 13:46:06 +0000

    add Materialize theme, add home-page, update base.html accordingly.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, redirect, request, url_for, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+#app is a variable (__name__) is a built in Flask variable. It needs this so it knows where to look for templates
 app = Flask(__name__)
 
 if path.exists("env.py"):
@@ -14,8 +15,11 @@ app.config["MONGODB_NAME"] = os.environ.get('MONGODB_NAME')
 
 mongo = PyMongo(app)
 
+@app.route("/")
+def index(): #this is the index 'view'
+    return render_template("index.html")
+
 # function for page to display a list all events
-@app.route('/')
 @app.route('/find_events')
 def find_events():
     return render_template("events.html", 
@@ -121,7 +125,7 @@ def add_sport():
     return render_template('add_sport.html')
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': #__main__ is the name of the default module in Python
     app.run(host=os.environ.get('IP'),
     port=int(os.environ.get('PORT')),
     debug=True)
