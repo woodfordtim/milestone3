4347ade983f5f755a4f287e2de82ecc8394d9a9c
woodfordtim <woodfordtim@gmail.com>
2020-09-15 21:17:44 +0000

    make minor changes html templates

--- test_events.py
+++ test_events.py
@@ -1,6 +1,14 @@
 import unittest
-import calc 
+from app import app
 
-class TestCalc(unittest.TestCalc):
-    def create_event():
+class TestRoutes(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        
+    def test_index_page_works(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 301)
+
+if __name__ == '__main__':
+    unittest.main()
 
